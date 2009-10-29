2641a9493996db956a5266dd711902346bfbe0ec
Craig Vermeer <craig@vermeers.net>
2009-10-29 14:47:40 -0400

    Dinner repository from book'

---
+++ Nerddinner/Models/DinnerRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace Nerddinner.Models
+{
+    public class DinnerRepository
+    {
+        private NerdDinnerDataContext db = new NerdDinnerDataContext();
+
+        public IQueryable<Dinner> FindAllDinners()
+        {
+            return db.Dinners;
+        }
+
+        public IQueryable<Dinner> FindUpcomingDinners()
+        {
+            return from dinner in db.Dinners
+                   where dinner.EventDate > DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+
+        }
+
+        public Dinner GetDinner(int id)
+        {
+            return db.Dinners.SingleOrDefault(d => d.DinnerID == id);
+        }
+
+        public void Add(Dinner dinner)
+        {
+            db.Dinners.InsertOnSubmit(dinner);
+        }
+
+        public void Delete(Dinner dinner)
+        {
+            db.RSVPs.DeleteAllOnSubmit(dinner.RSVPs);
+            db.Dinners.DeleteOnSubmit(dinner);
+        }
+
+        public void Save()
+        {
+            db.SubmitChanges();
+        }
+    }
+}
